09b3f97ec9da1fcff5f3febd5b4dcea87532c50b
Daisy-Dawn <agboella20@gmail.com>
2025-01-20 22:51:11 +0100

    header completed

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -1,7 +1,104 @@
-import React from 'react'
+'use client'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { RiCloseLargeLine } from 'react-icons/ri'
+import { usePathname } from 'next/navigation'
+import '../../app/globals.css'
 
 const Header = () => {
-    return <div>Header</div>
+    const pathname = usePathname() // Get the current path dynamically
+    const [toggle, setToggle] = useState(false)
+    const [componentMount, setComponentMount] = useState(false)
+
+    const handleToggle = () => {
+        setToggle(!toggle) // Toggle between showing and hiding the navbar
+        if (!componentMount) {
+            setComponentMount(true) // Set componentMount to true when toggle is first triggered
+        }
+    }
+
+    const navLinks = [
+        { title: 'Home', link: '/' },
+        { title: 'About', link: '/about' },
+        { title: 'Family Tree', link: '/family-tree' },
+        { title: 'Photos', link: '/photos' },
+    ]
+    return (
+        <div className="flex font-inter header sticky top-0 w-full z-[50] justify-center  items-center h-[100px] ">
+            {/* links */}
+            <div className="rounded-md w-1/2 hidden lg:flex justify-center p-[2px] gap-8 ">
+                {navLinks.map((link, index) => (
+                    <Link
+                        href={link.link}
+                        key={index}
+                        className={` capitalize text-center  ${
+                            pathname === link.link
+                                ? 'text-brown-gradient underline'
+                                : 'text-foreground'
+                        }`}
+                    >
+                        {link.title}
+                    </Link>
+                ))}
+            </div>
+
+            {/* RESPONSIVENESS */}
+
+            {/* {toggle && (
+                <div className="fixed inset-0 bg-black md:bg-opacity-50 bg-opacity-30"></div>
+            )} */}
+
+            <span
+                className="absolute right-[2rem] lg:hidden"
+                onClick={handleToggle}
+            >
+                {toggle ? (
+                    <RiCloseLargeLine />
+                ) : (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                        />
+                    </svg>
+                )}
+            </span>
+            {/*  */}
+            <div
+                className={`flex flex-col ${
+                    !componentMount && !toggle
+                        ? 'hidden'
+                        : toggle && componentMount
+                        ? 'slide-in-left'
+                        : 'slide-out-left'
+                } items-start justify-start glass-background opacity-50 top-0 bg-foreground text-appBrown2 absolute py-[38px] px-[20px] left-0  z-10 w-[65%] md:w-[45%] h-dvh nav shadow-lg transition-all duration-300 gap-4 lg:hidden`}
+            >
+                <div className="h-[50px]"></div>
+                {navLinks.map((link, index) => (
+                    <Link
+                        href={link.link}
+                        key={index}
+                        onClick={handleToggle}
+                        className={`my-[5px] capitalize text-center ${
+                            pathname === link.link
+                                ? 'text-brown-gradient underline '
+                                : 'text-appBrown'
+                        }`}
+                    >
+                        {link.title}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Header
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+                appBrown: '#8D6A4A',
+                appRed: '#D1495B',
+                appYellow: '#F4D35E',
+                appBrown2: '#83410F',
+            },
+            fontFamily: {
+                inter: ['Inter', 'serif'],
+                playfair: ['Playfair Display', 'serif'],
+            },
+        },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: [],
+} satisfies Config
