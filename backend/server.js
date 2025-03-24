b44b60d831e6e8c8eb8627b813bcc982fb8c3bf9
Daisy-Dawn <agboella20@gmail.com>
2025-03-24 13:04:57 +0100

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,44 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./src/config/db");
-const cors = require("cors");
-const familyRoutes = require("./src/routes/family");
-const photoRoutes = require("./src/routes/photos");
-require("./src/utils/debug");
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./src/config/db')
+const cors = require('cors')
+const familyRoutes = require('./src/routes/family')
+const photoRoutes = require('./src/routes/photos')
+require('./src/utils/debug')
 
-dotenv.config();
-connectDB();
+dotenv.config()
+connectDB()
 
-const app = express();
+const app = express()
+
+app.use(cors())
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        "https://omile-genealogy.vercel.app", // Production origin
-        "http://localhost:3000", // Development origin
-      ];
-
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // Allow credentials (cookies, headers, others)
-  })
-);
+    cors({
+        origin: '*', // Allows all origins
+        credentials: true, // Allows credentials (optional)
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+)
 
 // Routes
-app.use("/api/family", familyRoutes);
-app.use("/api/photos", photoRoutes);
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
+app.use('/api/family', familyRoutes)
+app.use('/api/photos', photoRoutes)
+app.get('/', (req, res) => {
+    res.send('Backend is running!')
+})
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  res.status(500).json({ error: err.message });
-});
+    res.status(500).json({ error: err.message })
+})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
